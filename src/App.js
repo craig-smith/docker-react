b093d523e26562d2fac67b7434a73560a17d15be
Craig Smith <poland_5@msn.com>
2019-03-14 09:53:17 +0100

    added config for aws deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fuck U
+            I am deployed in AWS!!
           </p>
           <a
             className="App-link"
